78e31e4cd3efd71fe5d5c8bcbd09a728f947967d
Kristoffer.Kodehode <kristofferb04.kodehode@gmail.com>
2024-02-14 08:57:55 +0100

    start settings menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 //import { useState } from 'react'
-import LayoutBase from "./assets/layout/layoutBase"
-import "./index.css"
+import LayoutBase from "./assets/layout/layoutBase";
+import "./index.css";
 
 function App() {
-  
   return (
     <>
+      <div>
+        <button>
+          <img src="src/assets/setting-cogwheel.svg" />
+        </button>
+      </div>
       <div id="container">
         <LayoutBase></LayoutBase>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
